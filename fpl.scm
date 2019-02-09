a8350f0682ce2a5a29f02980beeb1980194f3358
Rajjwal Rawal <rajjwal.rawal@bison.howard.edu>
2019-02-08 21:52:46 -0500

    solved question 1

--- fpl.scm
+++ fpl.scm
@@ -1,31 +1,18 @@
-; function that takes two simple lists as parameters,
-; returns #T if the two simple lists are equal #F otherwise
-(define (equalsimp lis1 lis2)
-  (cond
-    ((null? lis1) (null? lis2))
-    ((null? lis2) #F)
-    ((eq? (car lis1) (car lis2))
-     (equalsimp (cdr lis1)(cdr lis2)))
-    (else #F)
-  )
-)
-
-
-; function that takes a list and returns reversed version of L
-(define (reverse-general L)         
+; function that returns reveresed list if input is list else returns the input as it is
+(define (reversed L)         
   (cond                                          
-    ((null? L) L) ;return list, if empty                       
-    ((null? (cdr L)) L) ;return list, if only one element                       
-    (else                                        
-      (cons (car (reverse-general (cdr L))) ;recursively make reverse list
-            (reverse-general (cons (car L)
-                       (reverse-general (cdr (reverse-general (cdr L))))))
-      )
-    )
+    ((list? L) (reverse-general L))                                            
+    (else L)
   )
 )
 
 
-;(display (equalsimp '() (reverse-general '()))) ; check for ()
-;(display (equalsimp '(c b a) (reverse-general '(a b c)))) ; check for (a b c)
-;(display (equalsimp '(() b a) (reverse-general '(a b ())))) ; check for (() b c)
+; ; function that takes a list L and returns reversed version of L
+(define (reverse-general L)
+  (cond
+    ((null? L) '()) ; return L as it is if empty
+    (else (append (reverse-general(cdr L)) (list (reversed(car L))))) ; recursive call to generate reveresed
+   )
+)
+
+;(display(reverse-general '(1 (2 3) (4 (a (b (c d))))) ))
