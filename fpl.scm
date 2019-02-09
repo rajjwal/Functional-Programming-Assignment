62312f86fd64c7da1be7ee3d97b7fb4c05bcf8ee
Rajjwal Rawal <rajjwal.rawal@bison.howard.edu>
2019-02-09 10:00:28 -0500

    solved question 2

--- fpl.scm
+++ fpl.scm
@@ -16,3 +16,16 @@
 )
 
 ;(display(reverse-general '(1 (2 3) (4 (a (b (c d))))) ))
+
+
+; the function that returns the sum of only numbers in the list L
+(define (sum-up-numbers-simple  L)
+  (if (null? L) ; return 0 if no list
+    0
+  (if (number? (car L))
+    (+ (car L) (sum-up-numbers-simple  (cdr L))) ; add the element only if it is number
+    (+ 0 (sum-up-numbers-simple (cdr L)))) ; add 0 if the element is not number
+  )
+)
+
+;(display (sum-up-numbers-simple  '(a 100 b (200) c 300 d))) ; check for (a 100 b (200) c 300 d)
